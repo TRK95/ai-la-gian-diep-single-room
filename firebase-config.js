9ea957e2a96bc6760f5b5df695cac28921dcdb39
TRK95_nguyetanh <86118897+TRK95@users.noreply.github.com>
2026-08-01 15:48:59 +0700

    add config

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 // Dán cấu hình Firebase Web App của bạn vào đây.
 // Firebase Console -> Project settings -> Your apps -> SDK setup and configuration.
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_YOUR_DATABASE_URL.firebasedatabase.app",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyDWQrYLXreV4km9tZ_pVLxOxi_PWG5p25M",
+  authDomain: "ailagiandiep-f7257.firebaseapp.com",
+  databaseURL: "https://ailagiandiep-f7257-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ailagiandiep-f7257",
+  storageBucket: "ailagiandiep-f7257.firebasestorage.app",
+  messagingSenderId: "288365269575",
+  appId: "1:288365269575:web:ed1d0d049d4418ea61d762",
+  measurementId: "G-68TJCZ3W9E"
 };
 
 // Toàn bộ trang web chỉ sử dụng đúng một phòng cố định.
